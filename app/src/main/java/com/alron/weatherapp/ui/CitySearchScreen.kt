3117e07abefb55cb69d7437e3d2f8e5ff0b84e26
1alron <roma.niko393@gmail.com>
2025-07-13 13:39:10 +0300

    update: added back button for better UX

--- app/src/main/java/com/alron/weatherapp/ui/CitySearchScreen.kt
+++ app/src/main/java/com/alron/weatherapp/ui/CitySearchScreen.kt
@@ -13,15 +13,12 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
